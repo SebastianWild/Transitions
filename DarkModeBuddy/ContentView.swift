bd0cddc58e8fc4c77bbcd04c509abe21df802cca
Sebastian Wild <sebastianrwild@gmail.com>
2020-02-22 14:43:42 -0800

    Initial Commit

---
+++ DarkModeBuddy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DarkModeBuddy
+//
+//  Created by Sebastian Wild on 2/22/20.
+//  Copyright © 2020 Sebastian Wild. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
