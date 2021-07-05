06b051d1ce2841b94ad1744c855532a0647163aa
SebasWild <sebastian@swild.dev>
2021-07-05 14:00:13 -0700

    Add SystemBrightness utility to get screen brightness from IODisplayConnect

---
+++ Transitions/Utilities/SystemBrightness.swift
@@ -0,0 +1,28 @@
+//
+//  SystemBrightness.swift
+//  Transitions
+//
+//  Created by Sebastian Wild on 7/3/21.
+//  Copyright © 2021 Sebastian Wild. All rights reserved.
+//
+
+import Cocoa
+
+enum SystemBrightness {
+    static func getBrightness() -> Float {
+        var iterator: io_iterator_t = 0
+        var brightness: Float = 0.0
+        if IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IODisplayConnect"), &iterator) == kIOReturnSuccess {
+            var service: io_object_t = 1
+            while service != 0 {
+                service = IOIteratorNext(iterator)
+                IODisplayGetFloatParameter(service, 0, kIODisplayBrightnessKey as CFString, &brightness)
+                IOObjectRelease(service)
+
+                return brightness
+            }
+        }
+
+        return brightness
+    }
+}
