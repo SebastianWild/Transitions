8ca706d0aeac169b4238fce50ccc5f6baa72efd5
SebasWild <sebastian@swild.dev>
2021-08-10 07:14:59 +0200

    Make BirghtnessReadError conform to View
    This will show error messages as SwiftUI Text

---
+++ Transitions/Views/Errors/BrightnessReadError.swift
@@ -0,0 +1,17 @@
+//
+// Created by Sebastian Wild on 8/9/21.
+// Copyright (c) 2021 Sebastian Wild. All rights reserved.
+//
+
+import SwiftUI
+
+extension BrightnessReadError: View {
+    var body: some View {
+        switch self {
+        case let .readError(metadata, _):
+            Text("Could not read brightness from \(metadata.name)")
+        case .noDisplays:
+            Text("No displays that support brightness reading found.")
+        }
+    }
+}
