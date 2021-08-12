bf0a620e3457bfaa873187b138e77172ad2e4d9a
SebasWild <sebastian@swild.dev>
2021-08-12 16:48:51 +0200

    Add support for Enabled setting in PreferenceView
    It now actually does something!

--- Transitions/Controllers/TransitionsController.swift
+++ Transitions/Controllers/TransitionsController.swift
@@ -15,15 +15,34 @@ class TransitionsController {
 
     private let userData: UserData
     private var darkModeController: DarkModeController?
+    /// Will hold a subscriber listening on changes of the enabled status of the app
+    private var enabledCancellable: AnyCancellable?
+    /// Will hold a subscriber listening for display changes on the machine
     private var primaryDisplayUpdateCancellable: AnyCancellable?
 
     init(userData: UserData) {
         self.userData = userData
-
         setUp()
     }
 
     private func setUp() {
+        enabledCancellable = userData.$isAppEnabled
+            .sink { [weak self] isEnabled in
+                guard let self = self else { return }
+
+                if isEnabled {
+                    self.primaryDisplayUpdateCancellable = self.makeDisplayUpdatePipeline()
+                } else {
+                    self.primaryDisplayUpdateCancellable?.cancel()
+                }
+            }
+    }
+
+    /// Builds the Combine pipelines that listen on brightness, display, and threshold changes
+    /// and creates `DarkModeController`s that then toggle the system appearance as needed
+    ///
+    /// - Returns: `AnyCancellable` of the built pipeline
+    private func makeDisplayUpdatePipeline() -> AnyCancellable {
         let primaryDisplayChangedHandler = displayManager.$displays
             .throttle(for: 0.5, scheduler: RunLoop.main, latest: true)
             .map(\.first) // The first display is the primary display
@@ -35,7 +54,7 @@ class TransitionsController {
             .compactMap { $0 } // If there is no primary display, we cannot continue
 
         // Configure re-creating the DarkModeController when user preferences or displays change
-        primaryDisplayUpdateCancellable = Publishers.CombineLatest(primaryDisplayChangedHandler, userData.$interfaceStyleSwitchTriggerValue)
+        return Publishers.CombineLatest(primaryDisplayChangedHandler, userData.$interfaceStyleSwitchTriggerValue)
             .map { display, thresholdValue in
                 DarkModeController(display: display, threshold: thresholdValue)
             }
