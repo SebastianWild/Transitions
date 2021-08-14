32f190d642c6f24b0ecfcc662f5f59b1caee3676
SebasWild <sebastian@swild.dev>
2021-08-14 11:01:31 +0200

    Add Preferences view

---
+++ Transitions/Views/Preferences/Preferences.swift
@@ -0,0 +1,19 @@
+//
+//  Preferences.swift
+//  Transitions
+//
+//  Created by Sebastian Wild on 8/14/21.
+//  Copyright © 2021 Sebastian Wild. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct Preferences: View {
+    @EnvironmentObject private var userData: UserData
+    @EnvironmentObject private var controller: TransitionsController
+    
+    var body: some View {
+        EmptyView()
+    }
+}
