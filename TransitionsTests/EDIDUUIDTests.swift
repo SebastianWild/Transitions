7fdffb2ad0e57f940a02a5a90a0175fbd12d785d
SebasWild <sebastian@swild.dev>
2021-11-07 18:17:33 -0800

    Add unit test target

---
+++ TransitionsTests/EDIDUUIDTests.swift
@@ -0,0 +1,18 @@
+//
+//  TransitionsTests.swift
+//  TransitionsTests
+//
+//  Created by Sebastian Wild on 11/7/21.
+//  Copyright © 2021 Sebastian Wild. All rights reserved.
+//
+
+@testable import Transitions
+import XCTest
+
+class EDIDUUIDTests: XCTestCase {
+    let ediduuid = "4C2D9C0F-0000-0000-2B1C-0104B5772278"
+
+    func testGetVendorID() {
+        XCTAssertEqual(19501, ediduuid.vendorID)
+    }
+}
