711becab62c8965a31f2854e574666c7ff1bfed6
SebasWild <sebastian@swild.dev>
2021-11-11 12:21:17 -0800

    Initial fixes for macOS Monterey
    - LoginItem changes due to API changes. Need to
    look into not using deprecated APIs though…
    - Minor SwiftLint fix
    - Bridging header build error fix on Xcode 13.1
    - Bump deployment target to macOS 11

--- Transitions/Utilities/LoginItem.swift
+++ Transitions/Utilities/LoginItem.swift
@@ -21,9 +21,10 @@ enum LoginItem {
         }
         set {
             let list = LSSharedFileList.sessionLoginItems
+            let snapshot = list.copiedSnapshot
 
             guard newValue else {
-                guard let transitionsItem = list.copiedSnapshot.transitionsApp else { return }
+                guard let transitionsItem = snapshot.transitionsApp else { return }
                 LSSharedFileListItemRemove(list, transitionsItem)
 
                 return
@@ -37,7 +38,7 @@ enum LoginItem {
                 key: isHidden
             ] as CFDictionary
 
-            LSSharedFileListInsertItemURL(list, nil, nil, nil, Bundle.cfURL, properties, nil)
+            LSSharedFileListInsertItemURL(list, snapshot.last ?? kLSSharedFileListItemLast.takeRetainedValue(), nil, nil, Bundle.cfURL, properties, nil)
         }
     }
 }
@@ -48,19 +49,19 @@ private extension Bundle {
 
 private extension LSSharedFileList {
     static var sessionLoginItems: LSSharedFileList {
-        LSSharedFileListCreate(nil, kLSSharedFileListSessionLoginItems.takeRetainedValue(), nil).takeRetainedValue()
+        LSSharedFileListCreate(nil, kLSSharedFileListSessionLoginItems.takeRetainedValue(), nil)!.takeRetainedValue()
     }
 }
 
 private extension LSSharedFileList {
     var copiedSnapshot: [LSSharedFileListItem] {
-        LSSharedFileListCopySnapshot(self, nil).takeRetainedValue() as? [LSSharedFileListItem] ?? []
+        LSSharedFileListCopySnapshot(self, nil)!.takeRetainedValue() as? [LSSharedFileListItem] ?? []
     }
 }
 
 private extension Array where Element == LSSharedFileListItem {
     var transitionsApp: LSSharedFileListItem? {
-        filter { LSSharedFileListItemCopyResolvedURL($0, 0, nil).takeRetainedValue() == Bundle.cfURL }
+        filter { LSSharedFileListItemCopyResolvedURL($0, 0, nil)!.takeRetainedValue() == Bundle.cfURL }
             .first
     }
 }
