e8958bcfc8eec5d48afee8a67c51ef9632149a77
SebasWild <sebastian@swild.dev>
2021-11-11 12:59:26 -0800

    Refactor LoginItem implementation
    Remove reliance on deprecated APIs
    Use LaunchAtLogin package instead.

--- Transitions/Utilities/LoginItem.swift
+++ Transitions/Utilities/LoginItem.swift
@@ -4,64 +4,20 @@
 //
 
 import Foundation
+import LaunchAtLogin
 
 enum LoginItem {
-    // TODO: Error handling?
     /**
      Adds or removed the login item for Transitions.app.
 
-     pass `true` to
+     pass `true` to enable.
      */
     static var enabled: Bool {
         get {
-            LSSharedFileList
-                .sessionLoginItems
-                .copiedSnapshot
-                .transitionsApp != nil
+            LaunchAtLogin.isEnabled
         }
         set {
-            let list = LSSharedFileList.sessionLoginItems
-            let snapshot = list.copiedSnapshot
-
-            guard newValue else {
-                guard let transitionsItem = snapshot.transitionsApp else { return }
-                LSSharedFileListItemRemove(list, transitionsItem)
-
-                return
-            }
-
-            // Add the item
-            let key = Unmanaged.passUnretained(kLSSharedFileListLoginItemHidden.takeRetainedValue()).toOpaque()
-            let isHidden = Unmanaged.passUnretained(kCFBooleanFalse).toOpaque()
-            // see: https://stackoverflow.com/a/5598992
-            let properties = [
-                key: isHidden
-            ] as CFDictionary
-
-            LSSharedFileListInsertItemURL(list, snapshot.last ?? kLSSharedFileListItemLast.takeRetainedValue(), nil, nil, Bundle.cfURL, properties, nil)
+            LaunchAtLogin.isEnabled = newValue
         }
-    }
-}
-
-private extension Bundle {
-    static var cfURL: CFURL { URL(fileURLWithPath: Bundle.main.bundlePath) as CFURL }
-}
-
-private extension LSSharedFileList {
-    static var sessionLoginItems: LSSharedFileList {
-        LSSharedFileListCreate(nil, kLSSharedFileListSessionLoginItems.takeRetainedValue(), nil)!.takeRetainedValue()
-    }
-}
-
-private extension LSSharedFileList {
-    var copiedSnapshot: [LSSharedFileListItem] {
-        LSSharedFileListCopySnapshot(self, nil)!.takeRetainedValue() as? [LSSharedFileListItem] ?? []
-    }
-}
-
-private extension Array where Element == LSSharedFileListItem {
-    var transitionsApp: LSSharedFileListItem? {
-        filter { LSSharedFileListItemCopyResolvedURL($0, 0, nil)!.takeRetainedValue() == Bundle.cfURL }
-            .first
     }
 }
