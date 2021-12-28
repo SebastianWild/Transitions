54f8c09988ff13fa14c4e61ecdc2b454c13d663d
SebasWild <sebastian@swild.dev>
2021-12-27 18:20:43 -0800

    DDC WIP
    Add asyncMap extension to Publishers to allow async/await and Combine interoperability

--- Transitions/Extensions/Combine+Extensions.swift
+++ Transitions/Extensions/Combine+Extensions.swift
@@ -12,3 +12,16 @@ extension Publisher where Failure == Never {
         }
     }
 }
+
+extension Publisher {
+    func asyncMap<T>(_ transform: @escaping (Output) async -> T) -> Publishers.FlatMap<Future<T, Never>, Self> {
+        flatMap { value in
+            Future{ promise in
+                Task {
+                    let output = await transform(value)
+                    promise(.success(output))
+                }
+            }
+        }
+    }
+}
